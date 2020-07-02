48a0fd85ea9a817c73ada28d27dd6a96bd3e76c2
22BobbyW <22BobbyW@students.harker.org>
2020-07-01 23:32:01 -0700

    adding in model to app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
+import joblib
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Load ML model
+model = joblib.load('./notebooks/regr.pkl')
 
 @app.route('/')
 def index():
-    bestClassEver = 'Best Class Ever'
-    return render_template('index.html', bCE=bestClassEver)
+    # Make prediction - features = ['BEDS', 'BATHS', 'SQFT', 'AGE', 'LOTSIZE', 'GARAGE']
+    prediction = model.predict([[4, 2.5, 3005, 15, 17903.0, 1]])[0][0].round(1)
+    prediction = str(prediction)
+    return render_template('index.html', pred=prediction)
 
 
 @app.route('/world')
