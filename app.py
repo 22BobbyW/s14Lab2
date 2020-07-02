f2407adad3042ee2da877a2e2e6b03cfe569513c
22BobbyW <22BobbyW@students.harker.org>
2020-07-01 23:33:45 -0700

    adding in model to app.py

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Load ML model
-model = joblib.load('./notebooks/regr.pkl')
-
 @app.route('/')
 def index():
+    # Load ML model
+    model = joblib.load('./notebooks/regr.pkl')
+
     # Make prediction - features = ['BEDS', 'BATHS', 'SQFT', 'AGE', 'LOTSIZE', 'GARAGE']
     prediction = model.predict([[4, 2.5, 3005, 15, 17903.0, 1]])[0][0].round(1)
     prediction = str(prediction)
-    return render_template('index.html', pred=prediction)
+    return render_template('index.html', bCE=prediction)
 
 
 @app.route('/world')
