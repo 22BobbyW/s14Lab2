174ee988b12d2f33db732e0d90d800924b1225fa
22BobbyW <22BobbyW@students.harker.org>
2020-07-04 18:56:17 -0700

    interactive app

--- app.py
+++ app.py
@@ -1,22 +1,56 @@
-import joblib
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Load ML model
-#linearModel = joblib.load('./notebooks/regr.pkl')
+import joblib
+
+# load ML model
+linearModel = joblib.load('./notebooks/TTSModel.pkl')
+trainTestSplit = joblib.load('./notebooks/TTSModel.pkl')
+decisionTreeModel = joblib.load('./notebooks/DTModel.pkl')
+
+# the data
+beds = 4
+baths = 2.5
+sqft = 3005
+age = 15
+lotsize = 17903.0
+garage = 1
+reg = "linear"
 
 @app.route('/')
 def index():
-    bestClassEver = 'Best Class Ever'
-    return render_template('index.html', bCE=bestClassEver)
+    return render_template('index.html', prediction="", beds=beds, baths=baths, sqft=sqft, age=age, lotsize=lotsize, garages=garage, regression=reg)
 
 
-@app.route('/world')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/sending/data', methods=['GET'])
+def predict():
 
+    print(request.args)
 
-@app.route('/<you>')
-def hello_you(you):
-    return f'Hello, {you}!'
+    if request.method == 'GET':
+        beds = float(request.args['beds'])
+        baths = float(request.args['baths'])
+        sqft = float(request.args['sqft'])
+        age = float(request.args['age'])
+        lotsize = float(request.args['lotsize'])
+        garage = float(request.args['garages'])
+        reg = str(request.args['regressionType'])
+
+        if reg == "linear":
+            temp = linearModel.predict([[beds, baths, sqft, age, lotsize, garage]])[0][0].round(0)
+            pred = ' (linear regression): ' + str(temp)
+        elif reg == 'tree':
+            temp = decisionTreeModel.predict([[beds, baths, sqft, age, lotsize, garage]])[0].round(0)
+            pred = ' (decision tree regression): ' + str(temp)
+        elif reg == 'tts':
+            temp = trainTestSplit.predict([[beds, baths, sqft, age, lotsize, garage]])[0][0].round(0)
+            pred = ' (train test split linear regression): ' + str(temp)
+        else:
+            pred = ': you did not chose a regression model :('
+    else:
+        print('error')
+
+    prediction = "Housing Price Prediction" + str(pred)
+
+    return render_template('index.html', prediction=prediction, beds=beds, baths=baths, sqft=sqft, age=age, lotsize=lotsize, garages=garage, regression=reg)
